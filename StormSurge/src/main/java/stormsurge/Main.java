c3bdee60c4689eeed50361b096068cd6548570d6
Juho Kostet <juho.kostet@cs.helsinki.fi>
2015-03-12 02:08:43 +0200

    Add Sea and Ship classes

--- StormSurge/src/main/java/stormsurge/Main.java
+++ StormSurge/src/main/java/stormsurge/Main.java
@@ -4,16 +4,30 @@
  * and open the template in the editor.
  */
 
-package stormsurge.stormsurge;
+package stormsurge;
+
+import stormsurge.logic.Sea;
+import stormsurge.logic.Ship;
 
 /**
  *
- * @author jkostet
+ * @author Juho Kostet
+ */
+
+
+/** TODO:
+ *  - 2d grid class "The Sea!" to place things on
+ *      - Text based rendering
+ *  - Ship class
+ *  - 
+ * 
  */
 public class Main {
     
     public static void main(String [ ] args){
         System.out.println("Hello Sea!");
+        
+        
     }
     
     public void printHelloSea() {
---
+++ StormSurge/src/main/java/stormsurge/logic/Ship.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stormsurge.logic;
+
+/**
+ *
+ * @author jkostet
+ */
+public class Ship {
+    private int length;
+    private int health;
+
+    public Ship(int length) {
+        this.length = length;
+        this.health = length;
+    }
+    
+    @Override
+    public String toString() {
+        return "ship";
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    
+    
+    
+}
