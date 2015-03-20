c0387a9dd91d1b6e96a0020527cd4c7cbb40ba59
Juho Kostet <juho.kostet@cs.helsinki.fi>
2015-03-20 16:01:09 +0200

    Refactor test packages

--- src/test/java/stormsurge/entities/SeaTest.java
+++ src/test/java/stormsurge/entities/SeaTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package stormsurge;
+package stormsurge.entities;
 
 import static org.junit.Assert.*;
 import org.junit.Before;
